19889665d5d81b025d9785a727a8bf498c10db5b
FlowGoCrazy <thatflowgocrazy@gmail.com>
2024-05-16 16:56:41 -0400

    create / buy inst w/ persistent bc state

--- programs/rocket/src/lib.rs
+++ programs/rocket/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("8ppDaTFZgYJpPCrpxLow3Bq5HzZicQ6M63MGeXHPoEGb");
 
@@ -6,11 +7,65 @@ declare_id!("8ppDaTFZgYJpPCrpxLow3Bq5HzZicQ6M63MGeXHPoEGb");
 pub mod rocket {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("test 123");
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let bonding_curve = &mut ctx.accounts.bonding_curve;
+        bonding_curve.virtual_token_reserves = 0;
+        bonding_curve.virtual_sol_reserves = 0;
+        bonding_curve.real_token_reserves = 1_000_000_000;
+        bonding_curve.real_sol_reserves = 0;
+        bonding_curve.token_total_supply = 1_000_000_000;
+        bonding_curve.complete = false;
+
+        Ok(())
+    }
+
+    pub fn buy(ctx: Context<Buy>) -> ProgramResult {
+        let bonding_curve = &ctx.accounts.bonding_curve;
+        msg!(
+            "Virtual Token Reserves: {}",
+            bonding_curve.virtual_token_reserves
+        );
+        msg!(
+            "Virtual SOL Reserves: {}",
+            bonding_curve.virtual_sol_reserves
+        );
+        msg!("Real Token Reserves: {}", bonding_curve.real_token_reserves);
+        msg!("Real SOL Reserves: {}", bonding_curve.real_sol_reserves);
+        msg!("Token Total Supply: {}", bonding_curve.token_total_supply);
+        msg!("Complete: {}", bonding_curve.complete);
+
         Ok(())
     }
 }
 
+/* CONTEXT */
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = signer, space = 8 + BondingCurve::SIZE)]
+    pub bonding_curve: Account<'info, BondingCurve>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Buy<'info> {
+    #[account(mut)]
+    pub bonding_curve: Account<'info, BondingCurve>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+/* ACCOUNTS */
+#[account]
+pub struct BondingCurve {
+    pub virtual_token_reserves: u64, /* 8 bytes */
+    pub virtual_sol_reserves: u64,   /* 8 bytes */
+    pub real_token_reserves: u64,    /* 8 bytes */
+    pub real_sol_reserves: u64,      /* 8 bytes */
+    pub token_total_supply: u64,     /* 8 bytes */
+    pub complete: bool,              /* 1 byte */
+}
+impl BondingCurve {
+    pub const SIZE: usize = 8 + 8 + 8 + 8 + 8 + 1;
+}
--- tests/rocket.ts
+++ tests/rocket.ts
@@ -2,17 +2,65 @@ import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { Rocket } from '../target/types/rocket';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const loadPrivateKey = () => {
+    const privateKey = process.env.PRIVATE_KEY;
+    if (!privateKey) {
+        throw new Error('PRIVATE_KEY is not set');
+    }
+    return new Uint8Array(privateKey.split(', ').map((s) => parseInt(s, 10)));
+};
+
 describe('rocket', () => {
     anchor.setProvider(anchor.AnchorProvider.env());
+    const provider = anchor.getProvider();
 
     const program = anchor.workspace.Rocket as Program<Rocket>;
 
-    it('initializes', async () => {
-        const sig = await program.methods.initialize().rpc();
-        console.log('init sig:', sig);
+    const keypair = anchor.web3.Keypair.fromSecretKey(loadPrivateKey());
+    const wallet = new anchor.Wallet(keypair);
 
+    const bcKeypair = anchor.web3.Keypair.generate();
+    const bcWallet = new anchor.Wallet(bcKeypair);
+
+    it('initializes', async () => {
+        const createIx = await program.methods
+            .create()
+            .accounts({
+                bondingCurve: bcKeypair.publicKey,
+                signer: wallet.publicKey,
+            })
+            .instruction();
+
+        const tx = new anchor.web3.Transaction();
+        tx.add(createIx);
+
+        const buyIx = await program.methods
+            .buy()
+            .accounts({
+                bondingCurve: bcKeypair.publicKey,
+                signer: wallet.publicKey,
+            })
+            .instruction();
+        tx.add(buyIx);
+
+        /* set blockhash / fee payer */
+        const { blockhash, lastValidBlockHeight } = await provider.connection.getLatestBlockhash();
+        tx.recentBlockhash = blockhash;
+        tx.lastValidBlockHeight = lastValidBlockHeight;
+        tx.feePayer = keypair.publicKey;
+
+        /* sign tx with all signer accounts */
+        const payerSignedTx = await wallet.signTransaction(tx);
+        const bcSignedTx = await bcWallet.signTransaction(payerSignedTx);
+
+        /* send and confirm tx */
+        const sig = await anchor.getProvider().connection.sendRawTransaction(bcSignedTx.serialize());
         await anchor.getProvider().connection.confirmTransaction(sig, 'confirmed');
 
+        /* get confirmed tx result */
         const txData = await anchor.getProvider().connection.getParsedTransaction(sig, 'confirmed');
         console.log(txData.meta.logMessages);
     });
